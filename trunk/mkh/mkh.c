2220d820fbe3241f000e2891052377c37c57cd8f
FrancoisAncel <FrancoisAncel@ebd6e038-25dc-11df-a061-4f6072ac51cb>
2010-03-03 10:07:39 +0000

    Ajout de la bonne date dans le header

--- trunk/mkh/mkh.c
+++ trunk/mkh/mkh.c
@@ -57,11 +57,6 @@ int execfile(char *filename)
   return (EXIT_FAILURE);
 }
 
-int xfwrite(int fd, char *str)
-{
-  return (write(fd, str, strlen(str)));
-}
-
 int write_save(int fd, char *filename)
 {
   int i;
@@ -78,36 +73,5 @@ int write_save(int fd, char *filename)
   xfwrite(fd, "_INCLUDED\n# define ");
   xfwrite(fd, filename);
   xfwrite(fd, "_INCLUDED\n\n\n\n#endif\n");
-  return (EXIT_SUCCESS);
-}
-
-int write_header(int fd, char *filename)
-{
-  if (xfwrite(fd, "/*\n** ") < 0)
-    return (EXIT_FAILURE);
-  if (xfwrite(fd, filename) < 0)
-    return (EXIT_FAILURE);
-   if (xfwrite(fd, " for project in /u/all/") < 0)
-    return (EXIT_FAILURE);
- if (xfwrite(fd, LOGIN) < 0)
-    return (EXIT_FAILURE);
- if (xfwrite(fd, "/cu/travail/c/\n**\n** Made by ") < 0)
-    return (EXIT_FAILURE);
- if (xfwrite(fd, NAME) < 0)
-    return (EXIT_FAILURE);
- if (xfwrite(fd, "\n** Login   <") < 0)
-    return (EXIT_FAILURE);
- if (xfwrite(fd, LOGIN) < 0)
-    return (EXIT_FAILURE);
- if (xfwrite(fd, "@epitech.net>\n**\n** Started on  Wed Feb 24 14:52:36 2010 ") < 0)
-    return (EXIT_FAILURE);
- if (xfwrite(fd, NAME) < 0)
-    return (EXIT_FAILURE);
- if (xfwrite(fd, "\n** Last update Thu Feb 25 12:10:54 2010 ") < 0)
-    return (EXIT_FAILURE);
- if (xfwrite(fd, NAME) < 0)
-    return (EXIT_FAILURE);
- if (xfwrite(fd, "\n*/\n\n") < 0)
-    return (EXIT_FAILURE);
   return (EXIT_SUCCESS);
 }
